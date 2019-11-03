0d97036768045988a652a120fe13df9854cf8910
Herbert Abdillah <abdillah.herbert@gmail.com>
2019-11-04 01:35:29 +0700

    asdfff

---
+++ driver.js
@@ -0,0 +1,36 @@
+//a = new Bola(10, 200, 10);
+//b = new Bola(500, 200, 90);
+//a.dorong(4, 45);
+//b.dorong(4, 135);
+//loop(0);
+
+function simulasi(xa, ya, ma, xb, yb, mb, va, da, vb, db) {
+	a = new Bola(xa, ya, ma);
+	b = new Bola(xb, yb, mb);
+	a.dorong(va, da);
+	b.dorong(vb, db);
+	loop(0);
+}
+
+function mulai(){
+	apakahBerhenti = false;
+	var xa = Number(document.getElementById("xa").value);
+	var ya = Number(document.getElementById("ya").value);
+	var ma = Number(document.getElementById("ma").value);
+	var xb = Number(document.getElementById("xb").value);
+	var yb = Number(document.getElementById("yb").value);
+	var mb = Number(document.getElementById("mb").value);
+	var va = Number(document.getElementById("va").value);
+	var da = Number(document.getElementById("da").value);
+	var vb = Number(document.getElementById("vb").value);
+	var db = Number(document.getElementById("db").value);
+	console.log(xa, ya, ma, xb, yb, mb, va, da, vb, db);
+	simulasi(xa, ya, ma, xb, yb, mb, va, da, vb, db);
+}
+function berhenti(){
+	apakahBerhenti = true;
+}
+function contoh() {
+	apakahBerhenti = false;
+	simulasi(10, 200, 10, 500, 200, 90, 4, 45, 4, 135);
+}
--- proses.js
+++ proses.js
@@ -1,51 +1,49 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-ctx.canvas.height = window.innerHeight;
-ctx.canvas.width = window.innerWidth;
-class Bola{
-	constructor(x, y, radius) {
-		this.x  = x;
-		this.y  = y;
-		this.vx = 0;
-		this.vy = 0;
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+//ctx.canvas.height = window.innerHeight;
+//ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = 500;
+ctx.canvas.width = 800;
+class Bola {
+  constructor(x, y, radius) {
+    this.x = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
 
-		this.v = 0;
-		this.radius = radius;
-		this.m = radius;
-	}
-	dorong(v0, sudut) {
-		this.v = v0;
-		this.vx = v0 * Math.cos(sudut * Math.PI / 180);
-		this.vy = v0 * Math.sin(sudut * Math.PI / 180);
-	}
-	jalan() {
-		this.x = this.x + this.vx;
-		this.y = this.y + this.vy;
-	}
+    this.v = 0;
+    this.radius = radius;
+    this.m = radius;
+  }
+  dorong(v0, sudut) {
+    this.v = v0;
+    this.vx = v0 * Math.cos((sudut * Math.PI) / 180);
+    this.vy = v0 * Math.sin((sudut * Math.PI) / 180);
+  }
+  jalan() {
+    this.x = this.x + this.vx;
+    this.y = this.y + this.vy;
+  }
+  hitungV() {
+    var sq = Math.pow(this.vx, 2) + Math.pow(this.vy, 2);
+    var rt = Math.sqrt(sq);
+    return rt;
+  }
 }
 
-class Simulasi{
-	constructor(){
-		this.a = '';
-		this.b = '';
-	}
-}
-a = new Bola(10, 200, 10);
-b = new Bola(500, 200, 90);
 function hitung() {
-	a.jalan();
-	b.jalan();
+  a.jalan();
+  b.jalan();
 }
 function render() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.beginPath();
-	ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI);
-	ctx.stroke();
-	ctx.beginPath();
-	ctx.arc(b.x, b.y, b.radius, 0, 2 * Math.PI);
-	ctx.stroke();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(a.x, a.y, a.radius, 0, 2 * Math.PI);
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(b.x, b.y, b.radius, 0, 2 * Math.PI);
+  ctx.stroke();
 }
-i = 0;
 
 function deteksiTabrakan(p1x, p1y, r1, p2x, p2y, r2) {
   var a;
@@ -56,91 +54,108 @@ function deteksiTabrakan(p1x, p1y, r1, p2x, p2y, r2) {
   x = p1x - p2x;
   y = p1y - p2y;
 
-  if (a > Math.sqrt((x * x) + (y * y))) {
-	console.log('tabrakan');
+  if (a > Math.sqrt(x * x + y * y)) {
+    console.log('tabrakan');
     return true;
   } else {
     return false;
   }
-		}
+}
 function tabrakan() {
-	var m21,dvx2,aa,x21,y21,vx21,vy21,fy21,sign,vx_cm,vy_cm;
-	var m1, m2, x1, x2, y1, y2, vx1, vx2, vy1, vy2;
-	var R = 1;
-	m1 = a.m;
-	m2 = b.m;
-	x1 = a.x;
-	x2 = b.x;
-	y1 = a.y;
-	y2 = b.y;
-	vx1 = a.vx;
-	vx2 = b.vx;
-	vy1 = a.vy;
-	vy2 = a.vy;
-	       m21=m2/m1;
-       x21=x2-x1;
-       y21=y2-y1;
-       vx21=vx2-vx1;
-       vy21=vy2-vy1;
+  var m21, dvx2, aa, x21, y21, vx21, vy21, fy21, sign, vx_cm, vy_cm;
+  var m1, m2, x1, x2, y1, y2, vx1, vx2, vy1, vy2;
+  var R = 1;
+  m1 = a.m;
+  m2 = b.m;
+  x1 = a.x;
+  x2 = b.x;
+  y1 = a.y;
+  y2 = b.y;
+  vx1 = a.vx;
+  vx2 = b.vx;
+  vy1 = a.vy;
+  vy2 = a.vy;
+  m21 = m2 / m1;
+  x21 = x2 - x1;
+  y21 = y2 - y1;
+  vx21 = vx2 - vx1;
+  vy21 = vy2 - vy1;
 
-       vx_cm = (m1*vx1+m2*vx2)/(m1+m2) ;
-       vy_cm = (m1*vy1+m2*vy2)/(m1+m2) ;
+  vx_cm = (m1 * vx1 + m2 * vx2) / (m1 + m2);
+  vy_cm = (m1 * vy1 + m2 * vy2) / (m1 + m2);
 
+  //     *** return old velocities if balls are not approaching ***
+  if (vx21 * x21 + vy21 * y21 >= 0) return;
 
-//     *** return old velocities if balls are not approaching ***
-       if ( (vx21*x21 + vy21*y21) >= 0) return;
+  //     *** I have inserted the following statements to avoid a zero divide;
+  //         (for single precision calculations,
+  //          1.0E-12 should be replaced by a larger value). **************
 
+  fy21 = 1.0e-12 * Math.abs(y21);
+  //if ( fabs(x21)<fy21 ) {
+  //if (x21<0) { sign=-1; } else { sign=1;}
+  //x21=fy21*sign;
+  //}
 
-//     *** I have inserted the following statements to avoid a zero divide;
-//         (for single precision calculations,
-//          1.0E-12 should be replaced by a larger value). **************
+  if (Math.abs(x21) < fy21) {
+    if (x21 < 0) {
+      sign = -1;
+    } else {
+      sign = 1;
+    }
+    x21 = fy21 * sign;
+  }
+  //     ***  update velocities ***
+  aa = y21 / x21;
+  dvx2 = (-2 * (vx21 + aa * vy21)) / ((1 + aa * aa) * (1 + m21));
+  vx2 = vx2 + dvx2;
+  vy2 = vy2 + aa * dvx2;
+  vx1 = vx1 - m21 * dvx2;
+  vy1 = vy1 - aa * m21 * dvx2;
 
-       fy21=1.0E-12*Math.abs(y21);
-       //if ( fabs(x21)<fy21 ) {
-                   //if (x21<0) { sign=-1; } else { sign=1;}
-                   //x21=fy21*sign;
-        //}
+  //     ***  velocity correction for inelastic collisions ***
+  a.vx = (vx1 - vx_cm) * R + vx_cm;
+  a.vy = (vy1 - vy_cm) * R + vy_cm;
+  b.vx = (vx2 - vx_cm) * R + vx_cm;
+  b.vy = (vy2 - vy_cm) * R + vy_cm;
 
-	if(Math.abs(x21) < fy21) {
-		if(x21 < 0) {
-			sign = -1;
-		} else {
-			sign = 1;
-		}
-		x21 = fy21 * sign;
-	}
-//     ***  update velocities ***
-       aa=y21/x21;
-       dvx2= -2*(vx21 +aa*vy21)/((1+aa*aa)*(1+m21)) ;
-       vx2=vx2+dvx2;
-       vy2=vy2+aa*dvx2;
-       vx1=vx1-m21*dvx2;
-       vy1=vy1-aa*m21*dvx2;
-
-//     ***  velocity correction for inelastic collisions ***
-       a.vx=(vx1-vx_cm)*R + vx_cm;
-       a.vy=(vy1-vy_cm)*R + vy_cm;
-       b.vx=(vx2-vx_cm)*R + vx_cm;
-       b.vy=(vy2-vy_cm)*R + vy_cm;
-
-       return;
+  return;
 }
+var apakahberhenti = false;
 function loop(i) {
-	if(i > 10000) return;
-	setTimeout(function(){
-		hitung();
-		if(deteksiTabrakan(a.x, a.y, a.radius, b.x, b.y, b.radius)) {
-			tabrakan();
-			hitung();
-		}
-		console.log(a.vx + " " + a.vy + " " + b.vx + " " + b.vy);
-		render();
-		asdf(i+1);
-	}, 100);
+  if (apakahBerhenti) return;
+  setTimeout(function() {
+    hitung();
+    if (deteksiTabrakan(a.x, a.y, a.radius, b.x, b.y, b.radius)) {
+      tabrakan();
+      hitung();
+    }
+    console.log(
+      'a : v ',
+      a.hitungV(),
+      ' vx ',
+      a.vx,
+      ' vy ',
+      a.vy,
+      ' x ',
+      a.x,
+      ' y ',
+      a.y,
+    );
+    console.log(
+      'b : v ',
+      b.hitungV(),
+      ' vx ',
+      b.vx,
+      ' vy ',
+      b.vy,
+      ' x ',
+      a.x,
+      ' y ',
+      a.y,
+    );
+    console.log();
+    render();
+    loop(i + 1);
+  }, 100);
 }
-//a.dorong(2, 45);
-//b.dorong(4, -135);
-
-a.dorong(4, 45);
-b.dorong(4, 135);
-loop(0);
