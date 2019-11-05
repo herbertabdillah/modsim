1d91ade00c0fa3539ca34cc85d77754235632c7c
Herbert Abdillah <abdillah.herbert@gmail.com>
2019-11-05 08:11:37 +0700

    auto fill value

--- driver.js
+++ driver.js
@@ -29,6 +29,16 @@ function berhenti(){
 	apakahBerhenti = true;
 }
 function contoh() {
+	document.getElementById("xa").value = 300;
+	document.getElementById("ya").value = 100;
+	document.getElementById("ma").value = 10;
+	document.getElementById("xb").value = 500;
+	document.getElementById("yb").value = 100;
+	document.getElementById("mb").value = 20;
+	document.getElementById("va").value = 5;
+	document.getElementById("da").value = 45;
+	document.getElementById("vb").value = 5;
+	document.getElementById("db").value = 135;
 	apakahBerhenti = false;
 	simulasi(300, 100, 10, 500, 100, 20, 5, 45, 5, 135);
 }
